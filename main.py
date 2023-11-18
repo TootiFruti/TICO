1100623f754a06a96db06f5b070ded4f406fa6bb
TootiFruti <fake@mail.com>
2023-11-18 20:31:44 +0530

    TICO

--- main.py
+++ main.py
@@ -1,85 +1,27 @@
-import random
+from termcolor import colored
+from os import listdir
+from os.path import isdir
 
-def boardDraw(moves):
-    print(f"""
-     |      |  
-  {moves[0]}  |   {moves[1]}  |  {moves[2]}
-____ | ____ | ____ 
-     |      |
-  {moves[3]}  |   {moves[4]}  |  {moves[5]}
-____ | ____ | ____ 
-     |      |
-  {moves[6]}  |   {moves[7]}  |  {moves[8]}
-     |      |
+move = [
+    ['□', '□', '□', '□', '□', '□', '□'],
+    ['□', '□', '□', '□', '□', '□', '□'],
+    ['□', '□', '□', '□', '□', '□', '□'],
+    ['□', '□', '□', '□', '□', '□', '□'],
+    ['□', '□', '□', '□', '□', '□', '□'],
+    ['□', '□', '□', '□', '□', '□', '□'],
+    ['□', '□', '□', '□', '□', '□', '□']
+]
 
+move_column = {
+    "a": 0,
+    "b": 1,
+    "c": 2,
+    "d": 3,
+    "e": 4,
+    "f": 5,
+    "g": 6,
 
-    """) 
-    
-def computerMove(moves, mark, isGameLoaded):
-    if isGameLoaded[0]==1:
-        isGameLoaded[0]=0 
-        choice=None
-    else:
-        while 1:
-            choice=random.randint(0,8)
-            if moves[choice] != " ":
-                pass
-            else:
-                moves[choice]=mark
-                break
-    return choice
-def checkWinner(moves):
-    status = 0
-    player_won=None
-    if moves[0] == moves[1] == moves[2] and moves[0] != " ":
-        status = 1 
-        player_won=moves[0]
-
-    elif moves[3] == moves[4] == moves[5] and moves[3] != " ":
-        status = 1 
-        player_won=moves[3]
-
-    elif moves[6] == moves[7] == moves[8] and moves[6] != " ":
-        status = 1 
-        player_won=moves[6]
-    
-    elif moves[0] == moves[3] == moves[6] and moves[0] != " ":
-        status = 1 
-        player_won=moves[3]
-
-    elif moves[1] == moves[4] == moves[7] and moves[1] != " ":
-        status = 1 
-        player_won=moves[1]
-
-    elif moves[2] == moves[5] == moves[8] and moves[2] != " ":
-        status = 1 
-        player_won=moves[2]
-
-    elif moves[0] == moves[4] == moves[8] and moves[0] != " ":
-        status = 1 
-        player_won=moves[0]
-
-    elif moves[2] == moves[4] == moves[6] and moves[2] != " ":
-        status = 1 
-        player_won=moves[2]
-    
-    return (status, player_won)
-
-def checkDraw(moves):
-    draw_status=0
-    if moves.count(' ') == 0:
-        draw_status=1 
-    return draw_status
-
-def saveGame(moves, player_o, player_x):
-    gameData=[moves,player_o,player_x]
-    saveGameName= input("Enter game name : ")
-    filePath=f"./games/{saveGameName}.txt"
-    with open(filePath, "w") as f:
-        f.write(str(gameData))
-        f.close()
-    print(f"Game named : {saveGameName} saved successfully! Location: {filePath}")
-
+}
 
 logo = """
 
@@ -95,89 +37,215 @@ ______________________________________________________
 
 ______________________________________________________
 """
+player1 = "○"
+player2 = "x"
 
-if __name__=="__main__":
-    print(logo)
-    isGameLoaded=[0]
 
-    while 1:
-        choice =input("New game or load a game?(N/L) : ").replace(" ", "").lower()
-        if choice=="n":
-            moves=[' ',' ',' ',' ',' ', ' ', ' ', ' ', ' ']
-            print("You are [o] and Computer [x]")
-            player_o=input("Player [o] name? : ")
-            player_x=input("Player [x] name? : ")
-            break
-        elif choice=="l":
-            print("type the gamedata from the save files MANUALLY (im done with this thing and want this thing to be done, i should be studing for my exams and here i am doing this :/ )")
-            player_o=input("Player [o] name? : ")
-            player_x=input("Player [x] name? : ")
-            moves=eval(input("Enter the moves list(as a list) : "))
-            print("Saved game:- ")
-            boardDraw(moves)
-            isGameLoaded=[1]
-            break
-        else:
-            print(f"{choice}: Not a valid option! try again.")
+def drawBoard(move):
+    print(colored("  a b c d e f g", "yellow", attrs=["bold"]))
+    k = 1
+    for i in move:
+        t = colored(k, "yellow", attrs=["bold"])
+        k += 1
+        tmp = f"{t} "
+        for j in i:
+            if j == "□":
+                data = colored(j, "green", attrs=['bold'])
+            elif j == "○":
+                data = colored(j, "red", attrs=['bold'])
+            elif j == "x":
+                data = colored(j, "blue", attrs=['bold'])
 
-    print(f"\n{player_o} VS {player_x}\n")
-    while 1:
-        CM=computerMove(moves,"x",isGameLoaded)
-        if CM==None:
+            tmp += f"{data} "
+        print(tmp)
+
+
+def checkWinner(move):
+    winner = None
+    t = 0
+    for i in move:
+        try:
+            for j in range(len(i)):
+                if i[j] == player1:
+                    if player1 == i[j+1] == i[j+2] == i[j+3]:
+                        winner = player1
+                        break
+                    elif player1 == move[t+1][j] == move[t+2][j] == move[t+3][j]:
+                        winner = player1
+                        break
+                    elif player1 == move[t+1][j+1] == move[t+2][j+2] == move[t+3][j+3]:
+                        winner = player1
+                        break
+                    elif player1 == move[t+1][j-1] == move[t+2][j-2] == move[t+3][j-3]:
+                        winner = player1
+                        break
+                elif i[j] == player2:
+                    if player2 == i[j+1] == i[j+2] == i[j+3]:
+                        winner = player2
+                        break
+                    elif player2 == move[t+1][j] == move[t+2][j] == move[t+3][j]:
+                        winner = player2
+                        break
+                    elif player2 == move[t+1][j+1] == move[t+2][j+2] == move[t+3][j+3]:
+                        winner = player2
+                        break
+                    elif player2 == move[t+1][j-1] == move[t+2][j-2] == move[t+3][j-3]:
+                        winner = player2
+                        break
+        except IndexError:
             pass
-        else:
-            print(f"\n{player_x} move : {CM+1}")
-        boardDraw(moves)
-        status,winner= checkWinner(moves)
-        if status == 1:
-            if winner == "o":
-                winner=player_o
-            elif winner == "x":
-                winner=player_x
-            print(f"{winner} WON!")
-            exit()
-        elif checkDraw(moves) == 1:
-            print("GAME IS A DRAW!")
-            exit()
-        while 1:
-            while 1:
-                try:
-                    player_o_move=int(input("Enter your choice (box number. 1,2,..,9 or use 0 to exit ): "))
-                    if player_o_move == 0:
-                        saveGame(moves,player_o,player_x)
-                        exit()
-                    break
-                except ValueError:
-                    print("Choice must be an Integer. Please try again")
+        t += 1
+
+    return winner
+
+
+def checkDraw(move):
+    isDraw = False
+    t = 0
+    for i in move:
+        if i.count('□') == 0:
+            t += 1
+    if t == 7:
+        isDraw = True
+    return isDraw
+
+
+def playerMove(player, playerName):
+    while 1:
+        player_move = input(
+            f"{playerName} Enter your move : ").replace(" ", "").lower()
+        try:
+            j = move_column[player_move[0]]
             try:
-                if moves[player_o_move-1] != " ":
-                    print(f"{player_o_move}: is already taken. Choose a different box.")
+                if int(player_move[1]) == 0:
+                    print(f"{player_move[1]}: Invaild row choice")
+                    continue
                 else:
-                    moves[player_o_move-1]="o"
-                    print(f"Your move : {player_o_move}")
-                    break
+                    i = int(player_move[1]) - 1
+
+                if move[i][j] == "□":
+                    move[i][j] = player
+                else:
+                    print("Box is already occupied.")
+                winner = checkWinner(move)
+                if winner is not None:
+                    drawBoard(move)
+                    print(f"{winner} won!!")
+                    exit()
+                if checkDraw(move) is True:
+                    print("Game is a draw!")
+                    exit()
+                break
             except IndexError:
-                print(f"{player_o_move}: NOT A VAILD MOVE! Choose a move only from 1-9. Try Again.")
-            print(f"\n{player_o} move : {player_o_move}")
-            boardDraw(moves)
-
-        status,winner= checkWinner(moves)
-        if status == 1:
-            if winner == "o":
-                winner=player_o
-            elif winner == "x":
-                winner=player_x
-            boardDraw(moves)
-            print(f"{winner} WON!")
-            exit()
-
-        elif checkDraw(moves) == 1:
-            boardDraw(moves)
-            print("GAME IS A DRAW!")
-            exit()
-            
-        
+                print(f"{player_move[1]}: Invaild row choice")
+        except KeyError:
+            print(f"{player_move[0]}: Invaild column choice. ")
 
 
+def closing(moves, playerName1, playerName2, isGameLoaded, FileName, isPlayer1Turn, isPlayer2Turn):
+    if input("do you want to save the game? [y/N] ").lower().replace(" ", "") == "y":
+        while 1:
+            if isdir("./data/"):
+                try:
+                    if isGameLoaded[0] == 0:
+                        FileName = input("Enter the ganem's name : ")
+                    file_path = f"./data/{FileName}"
+                    data = [playerName1, playerName2,
+                            moves, isPlayer1Turn, isPlayer2Turn]
+                    with open(file_path, 'w') as f:
+                        f.write(str(data))
+                        f.close()
+                    print(f"Saved game as {file_path}. Quiting...")
+                    exit()
+
+                except Exception as e:
+                    print(e)
+                    print("Invaild file name. Choose something else.")
+            else:
+                print("\"data\" directory doesn't exist")
+                exit()
+    else:
+        exit()
 
 
+def run():
+    global move
+    isGameLoaded = [0]
+    filename = ''
+    isPlayer1Turn = True
+    isPlayer2Turn = True
+    try:
+        while True:
+            choice = input(
+                "Do you want to start a New game or load a game or q to quit ?[n/o/q] : ").replace(" ", "").lower()
+            if choice == "n":
+                playerName1 = input("Enter your name : ")
+                playerName2 = input("Enter the opponent's name : ")
+                if (input(f"Your name : {playerName1} and Opponent's {playerName2}. Want to continue? [Y\\n] : ").lower().replace(" ", "")) != "n":
+                    break
+                else:
+                    pass
+            elif choice == "o":
+                isGameLoaded[0] = 1
+                while 1:
+                    if isdir("./data/"):
+                        try:
+                            games = listdir("./data/")
+                            for i in range(len(games)):
+                                print(f"{i+1} : {games[i]}")
+                            try:
+                                choice = int(
+                                    input("Choose the game [1, 2, 3.. or 0 to exit] : "))
+                            except ValueError:
+                                print("Choice must be an integer.")
+                                continue
+                            gameFile = f"./data/{games[choice-1]}"
+                            filename = games[choice - 1]
+                            with open(gameFile, 'r') as f:
+                                gameData = f.readlines()[0]
+                                f.close()
+                            gameData = eval(gameData)
+                            playerName1 = gameData[0]
+                            playerName2 = gameData[1]
+                            move = gameData[2]
+                            isPlayer1Turn = gameData[3]
+                            isPlayer2Turn = gameData[4]
+                            break
+
+                        except IndexError:
+                            print("Not a vaild choice. Choose something else")
+                    else:
+                        print("\"data\" directory doesn't exits.")
+                        exit()
+                break
+            elif choice == "q":
+                exit()
+            else:
+                print(f"{choice} not a vaild option. (use ^C or q to quit )")
+    except KeyboardInterrupt:
+        exit()
+    print("(type moves like a1, b5, d6, etc and use ^C to exit)\n")
+    while 1:
+        try:
+            if isPlayer1Turn:
+                drawBoard(move)
+                playerMove(player1, playerName1)
+            else:
+                isPlayer1Turn = True
+        except KeyboardInterrupt:
+            closing(move, playerName1, playerName2, isGameLoaded,
+                    filename, isPlayer1Turn=True, isPlayer2Turn=False)
+        try:
+            if isPlayer2Turn:
+                drawBoard(move)
+                playerMove(player2, playerName2)
+            else:
+                isPlayer2Turn = True
+        except KeyboardInterrupt:
+            closing(move, playerName1, playerName2, isGameLoaded,
+                    filename, isPlayer1Turn=False, isPlayer2Turn=True)
+
+
+if __name__ == "__main__":
+    print(logo)
+    run()
